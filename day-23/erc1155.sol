474fed9169211d064acf145d737ae2a8a3db8cd6
Asharib Ali <asharibarain4@gmail.com>
2024-03-12 04:28:33 +0500

    update: add day-23

---
+++ day-23/erc1155.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Game is ERC1155, Ownable {
+    constructor(address initialOwner) ERC1155("") Ownable("initialOwner") {}
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data) public onlyOwner {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[])  public onlyOwner {
+        _mintBatch(to, ids, amounts, data);
+    }
+}
